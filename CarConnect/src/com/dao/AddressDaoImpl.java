a3c68a95d89c0c51391191b7a2dfb543b2f7686d
ANAND KARTHICK S <86352333+anandkarthik7@users.noreply.github.com>
2024-05-01 21:43:55 +0530

    Update AddressDaoImpl.java

--- CarConnect/src/com/dao/AddressDaoImpl.java
+++ CarConnect/src/com/dao/AddressDaoImpl.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.exception.DatabaseConnectionException;
+import com.exception.ResourceNotFoundException;
 import com.model.Address;
 import com.utility.DBConnection;
 
 public class AddressDaoImpl implements AddressDao{
 
 	@Override
-	public int save(Address address) throws SQLException, DatabaseConnectionException {
+	public int save(Address address) throws SQLException,DatabaseConnectionException {
 		Connection con=DBConnection.dbConnect();
 		String sql="INSERT INTO address (address_id,address_state , address_city, address_pincode) VALUES (?,?,?,?)";
 		PreparedStatement pstmt=con.prepareStatement(sql);
@@ -28,9 +29,9 @@ public class AddressDaoImpl implements AddressDao{
 	}
 
 	@Override
-	public boolean findOne(int address_id) throws SQLException, DatabaseConnectionException {
+	public boolean findOne(int address_id) throws SQLException,ResourceNotFoundException,DatabaseConnectionException {
 		Connection con = DBConnection.dbConnect();
-		String sql="select address_id from address where address_id=?";
+		String sql="select address_id from address where address_id=? and isActive='yes'";
 		//prepare the statement 
 		PreparedStatement pstmt = con.prepareStatement(sql);
 		pstmt.setInt(1, address_id);
@@ -53,16 +54,17 @@ public class AddressDaoImpl implements AddressDao{
 				DBConnection.dbClose();
 	}*/
 @Override
-	public int softDeleteById(int id) throws SQLException, DatabaseConnectionException {
+	public int softDeleteById(int id) throws SQLException,ResourceNotFoundException,DatabaseConnectionException {
 		Connection con = DBConnection.dbConnect();
-		String sql="update address SET isActive='no' where address_id =?";
+		String sql="update address set isActive='no' where address_id =?";
 		PreparedStatement pstmt = con.prepareStatement(sql);
 		pstmt.setInt(1, id);
 	int status = pstmt.executeUpdate();
 		DBConnection.dbClose();
 		return status;}
+
 	@Override
-	public List<Address> findAll() throws SQLException, DatabaseConnectionException {
+	public List<Address> findAll() throws SQLException,DatabaseConnectionException {
 		Connection con = DBConnection.dbConnect();
 		String sql="select * from address where isActive='yes'";
 		//prepare the statement 
@@ -82,9 +84,9 @@ public class AddressDaoImpl implements AddressDao{
 	}
 
 	@Override
-	public int updateById(Address address) throws SQLException, DatabaseConnectionException {
+	public int updateById(Address address) throws SQLException,ResourceNotFoundException,DatabaseConnectionException {
 	    Connection con = DBConnection.dbConnect();
-	    String sql = "UPDATE address SET address_state=?, address_city=?, address_pincode=? WHERE address_id=?";
+	    String sql = "update address set address_state=?, address_city=?, address_pincode=? WHERE address_id=?";
 	    PreparedStatement pstmt = con.prepareStatement(sql);
 	    pstmt.setString(1, address.getAddress_state());
 	    pstmt.setString(2, address.getAddress_city());
@@ -94,4 +96,19 @@ public class AddressDaoImpl implements AddressDao{
 		DBConnection.dbClose();
 		return status;}
 
+	@Override
+	public int getAddressIdByUserId(int id) throws SQLException, ResourceNotFoundException, DatabaseConnectionException {
+	    Connection con = DBConnection.dbConnect();
+	    String sql = "SELECT address_id FROM address WHERE address_id = (SELECT address_id FROM customer WHERE user_id = ? AND isActive = 'yes') UNION SELECT address_id FROM address WHERE address_id = (SELECT address_id FROM vendor WHERE user_id = ? AND isActive = 'yes')";
+	    PreparedStatement pstmt = con.prepareStatement(sql);
+	    pstmt.setInt(1, id);
+	    pstmt.setInt(2, id);
+	    ResultSet rst = pstmt.executeQuery();
+	    int address_id = 0;
+	    if (rst.next()) {
+	        address_id = rst.getInt("address_id");
+	    }
+	    DBConnection.dbClose();
+	    return address_id;
+	}
 }
